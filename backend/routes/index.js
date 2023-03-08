5993ac0dff582eedf33cc26e6741b4c438ecd9e8
bobomarinov <bobomarinov@gmail.com>
2023-03-08 17:24:20 +0200

    kubernetes integration

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express at '+ process.env.BACKEND_SERVICE_HOST });
 });
 
 module.exports = router;
