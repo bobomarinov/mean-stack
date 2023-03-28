ec75aecd9d490b4aa7d8b814eb2bc538239b4bf1
Boris Marinov <56587960+bobomarinov@users.noreply.github.com>
2023-03-28 17:04:01 +0300

    update

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,6 @@ var logger = require('morgan');
 var mongoose = require('mongoose');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 var studentsRouter = require('./routes/students');
 
 var app = express();
@@ -61,7 +60,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/students', studentsRouter);
 
 // catch 404 and forward to error handler
